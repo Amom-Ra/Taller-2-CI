629a59b96e391f075826e5d9fc5fa4b1e8db2b4b
Amom-Ra <fabian.gonzalez@outlook.com>
2026-04-25 04:47:26 -0500

    fix: formatear app.py con black

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ from .calculadora import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     resultado = None
@@ -29,7 +28,6 @@ def index():
             resultado = "Error: No se puede dividir por cero"
 
     return render_template("index.html", resultado=resultado)
-
 
 if __name__ == "__main__":  # pragma: no cover
     app.run(debug=True, port=5000, host="127.0.0.1")
